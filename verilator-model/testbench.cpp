0cd39cb49a4da124310a753f07e21d5b317a4074
Ian Bolton <ian.bolton@embecosm.com>
2017-06-27 16:19:27 +0100

    Attempt to implement halting and single-stepping
    The implementation is inspired by the spi_debug_test.svh and tb.svh
    implementations from PULPino.
    
    However, something is wrong - as soon as we try writing to DBG_CTRL to
    set DBG_CTRL_HALT, the core thinks there's been an illegal instruction.
    Sample output:
    
        DBG_CTRL  0
        DBG_HIT   0
                    1280: Entering exception routine.
                    1280: Illegal instruction (core 0) at PC 0x000000d0:
        ...
        <further output follows from attempts to single step>

--- verilator-model/testbench.cpp
+++ verilator-model/testbench.cpp
@@ -24,55 +24,156 @@
 #include "Vtop_ram__A10.h"
 #include "Vtop_dp_ram__A10.h"
 
+#include <iostream>
+#include <cstdint>
+#include <cstdlib>
+
+using std::cerr;
+using std::endl;
+
+bool USE_DEBUGGER = true;
 
 // Count of clock ticks
 
 static vluint64_t  cpuTime = 0;
 
+// Debug registers
 
-int
-main (int    argc,
-      char * argv[])
+const uint16_t DBG_CTRL    = 0x0000;  //!< Debug control
+const uint16_t DBG_HIT     = 0x0004;  //!< Debug hit
+const uint16_t DBG_IE      = 0x0008;  //!< Debug interrupt enable
+const uint16_t DBG_CAUSE   = 0x000c;  //!< Debug cause (why entered debug)
+const uint16_t DBG_GPR0    = 0x0400;  //!< General purpose register 0
+const uint16_t DBG_GPR31   = 0x047c;  //!< General purpose register 41
+const uint16_t DBG_NPC     = 0x2000;  //!< Next PC
+const uint16_t DBG_PPC     = 0x2004;  //!< Prev PC
+
+// Debug register flags
+
+// FIXME: Changed to uint32_t to get to compile - constants too large. Check
+// the constants are correct.
+const uint32_t DBG_CTRL_HALT = 0x00010000;    //!< Halt core
+const uint32_t DBG_CTRL_SSTE = 0x00000001;    //!< Single step core
+
+// GDB register numbers
+
+const int REG_R0  = 0;                //!< GDB R0 regnum
+const int REG_R31 = 31;               //!< GDB R31 regnum
+
+static uint64_t mCycleCnt = 0;
+
+Vtop *cpu;
+VerilatedVcdC * tfp;
+
+void clockSpin(uint32_t cycles)
 {
-  Vtop * cpu = new Vtop;
-  VerilatedVcdC * tfp;
-
-  // Open VCD
-
-  Verilated::traceEverOn (true);
-  tfp = new VerilatedVcdC;
-  cpu->trace (tfp, 99);
-  tfp->open ("model.vcd");
-
-  // Fix some signals for now.
-
-  cpu->irq_i          = 0;
-  cpu->debug_req_i    = 0;
-  cpu->fetch_enable_i = 1;
-
-  // Cycle through reset
-
-  cpu->rstn_i = 0;
-
-  for (int i = 0; i < 100; i++)
-    {
+  for (uint32_t i = 0; i < cycles; i++)
+  {
       cpu->clk_i = 0;
       cpu->eval ();
       cpuTime += 5;
       tfp->dump (cpuTime);
-
       cpu->clk_i = 1;
       cpu->eval ();
       cpuTime += 5;
       tfp->dump (cpuTime);
-    }
+      mCycleCnt++;
+  }
+}
 
+void debugAccess(uint32_t addr, uint32_t& val, bool write_enable)
+{
+  cpu->debug_req_i   = 1;
+  cpu->debug_addr_i  = addr;
+  cpu->debug_we_i    = write_enable ? 1 : 0;
+
+  if (write_enable)
+  {
+    cpu->debug_wdata_i = val;
+  }
+
+  // Access has been acknowledged when we get the grant signal asserted.
+  do
+    {
+      clockSpin(1);
+    }
+  while (cpu->debug_gnt_o == 0);
+
+  // Don't need to request once we get the grant.
+  cpu->debug_req_i = 0;
+
+  if (!write_enable)
+  {
+    // For reads, we need to read the data when we get rvalid_o.
+    // This could be on the same cycle as the grant, or later.
+    while (cpu->debug_rvalid_o == 0)
+    {
+      clockSpin(1);
+    }
+    val = cpu->debug_rdata_o;
+  }
+}
+
+uint32_t debugRead(uint32_t addr)
+{
+  uint32_t val;
+  debugAccess(addr, val, false);
+  return val;
+}
+
+void debugWrite(uint32_t addr, uint32_t val)
+{
+  debugAccess(addr, val, true);
+}
+
+void waitForDebugHit()
+{
+  // Keep reading the DBG_HIT register until it is SSTH
+  uint32_t dbg_hit;
+  do {
+      dbg_hit = debugRead(DBG_HIT);
+      std::cout << "DBG_HIT reg " << std::hex << dbg_hit << std::dec << std::endl;
+  } while (!(dbg_hit & 1));
+}
+
+// Assumes stall could happen at any point - don't need to wait a cycle to check
+void waitForDebugStall()
+{
+  while (!(debugRead(DBG_CTRL) & DBG_CTRL_HALT))
+  {
+    clockSpin(1);
+  }
+}
+
+void stepSingle ()
+{
+  std::cout << "DBG_CTRL  " << std::hex << debugRead(DBG_CTRL) << std::dec << std::endl;
+  std::cout << "DBG_HIT   " << std::hex << debugRead(DBG_HIT) << std::dec << std::endl;
+  std::cout << "DBG_CAUSE " << std::hex << debugRead(DBG_CAUSE) << std::dec << std::endl;
+  std::cout << "DBG_NPC   " << std::hex << debugRead(DBG_NPC) << std::dec << std::endl;
+  std::cout << "DBG_PPC   " << std::hex << debugRead(DBG_PPC) << std::dec << std::endl;
+
+  // Clear DBG_HIT
+  //debugWrite(DBG_HIT, 0);
+
+  // Write SSTE into the debug register
+  debugWrite(DBG_CTRL, DBG_CTRL_HALT | DBG_CTRL_SSTE);
+  debugWrite(DBG_CTRL, DBG_CTRL_SSTE);
+
+  waitForDebugStall();
+
+  // Halted again?
+}
+
+void loadProgram()
+{
   // Write some program code into memory:
   //
   // ; Store a word to memory first:
   // li a5, 64
   // li a4, 102
   // sw a4, 0(a5)
+  // ; Repeated <repeat_factor> times (20 at present)
   //
   // ; Then do something a bit like _exit(0)
   // li a1, 0
@@ -83,62 +184,115 @@ main (int    argc,
   //
   // Execution begins at 0x80, so that's where we write our code.
 
-  cpu->top->ram_i->dp_ram_i->writeByte (0x80, 0x93);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x81, 0x07);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x82, 0x00);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x83, 0x04);
+  uint32_t addr = 0x80;
+  uint32_t repeat_factor = 20;
+  for (size_t i = 0; i < repeat_factor; i++)
+  {
+    cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x0, 0x93);
+    cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x1, 0x07);
+    cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x2, 0x00);
+    cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x3, 0x04);
 
-  cpu->top->ram_i->dp_ram_i->writeByte (0x84, 0x13);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x85, 0x07);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x86, 0x60);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x87, 0x06);
+    cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x4, 0x13);
+    cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x5, 0x07);
+    cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x6, 0x60);
+    cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x7, 0x06);
 
-  cpu->top->ram_i->dp_ram_i->writeByte (0x88, 0x23);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x89, 0xa0);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x8a, 0xe7);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x8b, 0x00);
+    cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x8, 0x23);
+    cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x9, 0xa0);
+    cpu->top->ram_i->dp_ram_i->writeByte (addr + 0xa, 0xe7);
+    cpu->top->ram_i->dp_ram_i->writeByte (addr + 0xb, 0x00);
 
-  cpu->top->ram_i->dp_ram_i->writeByte (0x8c, 0x93);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x8d, 0x05);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x8e, 0x00);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x8f, 0x00);
+    addr += 0xC;
+  }
 
-  cpu->top->ram_i->dp_ram_i->writeByte (0x90, 0x13);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x91, 0x06);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x92, 0x00);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x93, 0x00);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x0, 0x93);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x1, 0x05);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x2, 0x00);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x3, 0x00);
 
-  cpu->top->ram_i->dp_ram_i->writeByte (0x94, 0x93);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x95, 0x06);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x96, 0x00);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x97, 0x00);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x4, 0x13);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x5, 0x06);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x6, 0x00);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x7, 0x00);
 
-  cpu->top->ram_i->dp_ram_i->writeByte (0x98, 0x93);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x99, 0x08);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x9a, 0xd0);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x9b, 0x05);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x8, 0x93);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x9, 0x06);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0xa, 0x00);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0xb, 0x00);
 
-  cpu->top->ram_i->dp_ram_i->writeByte (0x9c, 0x73);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x9d, 0x00);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x9e, 0x00);
-  cpu->top->ram_i->dp_ram_i->writeByte (0x9f, 0x00);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0xc, 0x93);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0xd, 0x08);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0xe, 0xd0);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0xf, 0x05);
+
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x10, 0x73);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x11, 0x00);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x12, 0x00);
+  cpu->top->ram_i->dp_ram_i->writeByte (addr + 0x13, 0x00);
+}
+
+int
+main (int    argc,
+      char * argv[])
+{
+  cpu = new Vtop;
+
+  // Open VCD
+
+  Verilated::traceEverOn (true);
+  tfp = new VerilatedVcdC;
+  cpu->trace (tfp, 99);
+  tfp->open ("model.vcd");
+
+  // Fix some signals for now.
+  cpu->irq_i          = 0;
+  cpu->debug_req_i    = 0;
+  cpu->fetch_enable_i = 0;
+
+  // Cycle through reset
+  cpu->rstn_i = 0;
+  clockSpin(100);
+  cpu->rstn_i = 1;
+
+  // Put a few instructions at the boot address
+  loadProgram();
 
   // Do some ordinary clocked logic.
 
-  cpu->rstn_i = 1;
+  if (USE_DEBUGGER)
+  {
+    cpu->fetch_enable_i = 1;
 
-  for (int i = 0; i < 100; i++)
-    {
-      cpu->clk_i = 0;
-      cpu->eval ();
-      cpuTime += 5;
-      tfp->dump (cpuTime);
+    // Run for a few cycles normally
+    clockSpin(20);
 
-      cpu->clk_i = 1;
-      cpu->eval ();
-      cpuTime += 5;
-      tfp->dump (cpuTime);
+    // Try to halt the CPU in the same way as in spi_debug_test.svh
+    debugWrite(DBG_CTRL, debugRead(DBG_CTRL) | DBG_CTRL_HALT);
+    debugWrite(DBG_IE, 0xF);
+    waitForDebugStall();
+    debugWrite(DBG_CTRL, debugRead(DBG_CTRL) & ~DBG_CTRL_HALT);
+
+    // Try and step 5 instructions
+    for (int j=0; j<5; j++) {
+      stepSingle ();
     }
+  } else {
+    cpu->fetch_enable_i = 1;
+
+    for (int i = 0; i < 100; i++)
+      {
+        cpu->clk_i = 0;
+        cpu->eval ();
+        cpuTime += 5;
+        tfp->dump (cpuTime);
+
+        cpu->clk_i = 1;
+        cpu->eval ();
+        cpuTime += 5;
+        tfp->dump (cpuTime);
+      }
+  }
 
   // Close VCD
 
